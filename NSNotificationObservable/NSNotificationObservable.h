74c83c61589d65cb35afce8ade59862c622d3663
sgr-ksmt <melodydance.k.s@gmail.com>
2016-08-01 14:27:50 +0900

    prepare project

---
+++ NSNotificationObservable/NSNotificationObservable.h
@@ -0,0 +1,19 @@
+//
+//  NSNotificationObservable.h
+//  NSNotificationObservable
+//
+//  Created by Suguru Kishimoto on 8/1/16.
+//  Copyright © 2016 Suguru Kishimoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NSNotificationObservable.
+FOUNDATION_EXPORT double NSNotificationObservableVersionNumber;
+
+//! Project version string for NSNotificationObservable.
+FOUNDATION_EXPORT const unsigned char NSNotificationObservableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSNotificationObservable/PublicHeader.h>
+
+
